37e6cee3d4d9540d71e1af097bd5c93faf41b50b
SIVVEN <ivanitamaxim286@gmail.com>
2026-05-31 21:45:03 +0300

    2145

--- src/Product.jsx
+++ src/Product.jsx
@@ -110,154 +110,154 @@ export default function Product() {
               "0 10px 24px rgba(0,0,0,0.16)",
           }}
         >
-        <TransformWrapper
-          initialScale={1}
-          minScale={1}
-          maxScale={2.5}
-          centerOnInit
-          limitToBounds
-          panning={{ disabled: true }}
-          pinch={{ step: 5 }}
-          doubleClick={{ disabled: true }}
-          wheel={{ disabled: true }}
-        >
-          {({ resetTransform }) => (
-            <div
-              onTouchEnd={() => {
-                setTimeout(() => {
-                  resetTransform(200);
-                }, 120);
-              }}
-            >
-              <TransformComponent>
-                <div
-                  style={{
-                    position: "relative",
-                    width: "100%",
-                    maxWidth: "260px",
-                  }}
-                >
-                  <img
-                    src="/bottle.png"
-                    alt="Glifosat"
+          <TransformWrapper
+            initialScale={1}
+            minScale={1}
+            maxScale={2.5}
+            centerOnInit
+            limitToBounds
+            panning={{ disabled: true }}
+            pinch={{ step: 5 }}
+            doubleClick={{ disabled: true }}
+            wheel={{ disabled: true }}
+          >
+            {({ resetTransform }) => (
+              <div
+                onTouchEnd={() => {
+                  setTimeout(() => {
+                    resetTransform(200);
+                  }, 120);
+                }}
+              >
+                <TransformComponent>
+                  <div
                     style={{
+                      position: "relative",
                       width: "100%",
-                      objectFit: "contain",
-                      borderRadius: "22px",
-                      display: "block",
-
-                      transition: "transform 0.25s ease",
-
-                      filter:
-                        "drop-shadow(0 8px 14px rgba(0, 0, 0, 0.01))",
-
+                      maxWidth: "260px",
                     }}
-                  />
-                </div>
-              </TransformComponent>
-            </div>
-          )}
-        </TransformWrapper>
+                  >
+                    <img
+                      src="/bottle.png"
+                      alt="Glifosat"
+                      style={{
+                        width: "100%",
+                        objectFit: "contain",
+                        borderRadius: "22px",
+                        display: "block",
 
-        <div
-          style={{
-            position: "absolute",
-            bottom: "16px",
-            left: "22px",
-            right: "22px",
+                        transition: "transform 0.25s ease",
 
-            textAlign: "center",
+                        filter:
+                          "drop-shadow(0 8px 14px rgba(0, 0, 0, 0.01))",
 
-            fontSize: "10.5px",
-            lineHeight: 1.35,
+                      }}
+                    />
+                  </div>
+                </TransformComponent>
+              </div>
+            )}
+          </TransformWrapper>
 
-            color: "rgba(255,255,255,0.72)",
+          <div
+            style={{
+              position: "absolute",
+              bottom: "16px",
+              left: "22px",
+              right: "22px",
 
-            fontWeight: 500,
-          }}
-        >
-          *Изображение носит ознакомительный характер.
-          Упаковка и производитель могут отличаться.
-        
-        </div>
+              textAlign: "center",
+
+              fontSize: "10.5px",
+              lineHeight: 1.35,
+
+              color: "rgba(255,255,255,0.72)",
+
+              fontWeight: 500,
+            }}
+          >
+            *Изображение носит ознакомительный характер.
+            Упаковка и производитель могут отличаться.
+
+          </div>
 
         </div>
       </div>
 
-            {/* Инструкция / Отзывы */}
-            <div
-              style={{
-                marginTop: "28px",
+      {/* Инструкция / Отзывы */}
+      <div
+        style={{
+          marginTop: "28px",
 
-                width: "100%",
-                height: "68px",
+          width: "100%",
+          height: "68px",
 
-                display: "flex",
-                alignItems: "center",
+          display: "flex",
+          alignItems: "center",
 
-                gap: "12px",
-              }}
-            >
+          gap: "12px",
+        }}
+      >
 
-            {/* Инструкция */}
-            <button
-            onClick={() => navigate("/info")}
-              style={{
-                flex: 1,
-                height: "100%",
+        {/* Инструкция */}
+        <button
+          onClick={() => navigate("/info")}
+          style={{
+            flex: 1,
+            height: "100%",
 
-                border: "1px solid rgba(255,255,255,0.04)",
+            border: "1px solid rgba(255,255,255,0.04)",
 
-                background:
-                     "linear-gradient(180deg, rgba(108,94,72,0.58) 0%, rgba(74,64,48,0.72) 100%)",
+            background:
+              "linear-gradient(180deg, rgba(108,94,72,0.58) 0%, rgba(74,64,48,0.72) 100%)",
 
-                color: "#FFFFFF",
+            color: "#FFFFFF",
 
-                fontSize: "17px",
-                fontWeight: 600,
+            fontSize: "17px",
+            fontWeight: 600,
 
-                cursor: "pointer",
+            cursor: "pointer",
 
-                boxShadow:
-                  "0 8px 22px rgba(0,0,0,0.12)",
+            boxShadow:
+              "0 8px 22px rgba(0,0,0,0.12)",
 
-                borderRadius: "22px",
-                margin: "6px",
-                backdropFilter: "blur(12px)",
-              }}
-            >
-              Информация
-            </button>
+            borderRadius: "22px",
+            margin: "6px",
+            backdropFilter: "blur(12px)",
+          }}
+        >
+          Информация
+        </button>
 
-            {/* Отзывы */}
-            <button
-              onClick={() => navigate("/reviews")}
-              style={{
-                flex: 1,
-                height: "100%",
+        {/* Отзывы */}
+        <button
+          onClick={() => navigate("/reviews")}
+          style={{
+            flex: 1,
+            height: "100%",
 
-                border: "1px solid rgba(255,255,255,0.04)",
+            border: "1px solid rgba(255,255,255,0.04)",
 
-                background: 
-                  "linear-gradient(180deg, rgba(108,94,72,0.58) 0%, rgba(74,64,48,0.72) 100%)",
-                color: "#F3F4F6",
+            background:
+              "linear-gradient(180deg, rgba(108,94,72,0.58) 0%, rgba(74,64,48,0.72) 100%)",
+            color: "#F3F4F6",
 
-                fontSize: "17px",
-                fontWeight: 600,
+            fontSize: "17px",
+            fontWeight: 600,
 
-                boxShadow:
-                  "0 8px 22px rgba(0,0,0,0.12)",
+            boxShadow:
+              "0 8px 22px rgba(0,0,0,0.12)",
 
-                borderRadius: "22px",
-                margin: "6px",
-                backdropFilter: "blur(12px)",
+            borderRadius: "22px",
+            margin: "6px",
+            backdropFilter: "blur(12px)",
 
-                cursor: "pointer",
-              }}
-            >
-              Отзывы
-            </button>
-          </div>
+            cursor: "pointer",
+          }}
+        >
+          Отзывы
+        </button>
+      </div>
 
       {/* Цена + кнопка */}
       <div
@@ -290,8 +290,8 @@ export default function Product() {
             250 MDL
           </div>
         </div>
-
         <button
+          onClick={() => navigate("/checkout")}
           style={{
             flex: 1,
             height: "68px",
