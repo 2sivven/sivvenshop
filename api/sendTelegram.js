1a3952b1a77934c4959c05849528218f3cbf8506
SIVVEN <ivanitamaxim286@gmail.com>
2026-06-01 20:58:37 +0300

    Create sendTelegram.js

---
+++ api/sendTelegram.js
@@ -0,0 +1,31 @@
+export default async function handler(req, res) {
+  try {
+    const { text } = req.body;
+
+    const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+    const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+    await fetch(
+      `https://api.telegram.org/bot${8585592932:AAERgtJSkvassa4g1_euBeLIb7BxPaDeU6c}/sendMessage`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: CHAT_ID,
+          text,
+        }),
+      }
+    );
+
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+}
